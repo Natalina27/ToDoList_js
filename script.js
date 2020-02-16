b8d73a20c1129828b6aff43d1db34b949afcc5cb
Natalya Myunster <natalyamyunster@yahoo.com>
2020-02-16 13:21:56 +0100

    added read function for reading from input by click added element to the list

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const buttonAdd = document.createElement('button');
 const mainContainer = document.querySelector('.wrap__container');
 
+
 mainContainer.appendChild(buttonAdd);
-buttonAdd.textContent = 'Click me';
+buttonAdd.textContent = 'ADD TO THE LIST';
 buttonAdd.classList.add('button-add');
 
+function readInputValue() {
+    return document.querySelector('.insert-input').value;
+}
 
 function onClick() {
-    console.log('Hello');
+    let listElement = document.createElement('li');
+    let node = document.createTextNode(readInputValue());
+    listElement.appendChild(node);
+
+    let ourList = document.querySelector('.list');
+    ourList.appendChild(listElement);
 }
 
 buttonAdd.addEventListener('click', onClick);
